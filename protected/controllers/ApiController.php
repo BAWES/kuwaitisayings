2abcac550830c29b07199139f86234cf69c43e2b
Khalid Al-Mutawa <contact@bawes.net>
2015-01-02 15:56:31 +0300

    Added hash output based on the last time application was updated. This will be useful for users so they don't have to download data regularly (if they are up to date) - and will reduce stress on server

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -3,6 +3,26 @@
 class ApiController extends Controller {
 
     /**
+     * Get MD5 Hash of Last Updated
+     */
+    public function actionLastUpdated() {
+        header("Content-Type: application/json; charset=utf-8");
+        
+        if(isset($_POST["password"])){
+            if($_POST["password"] != "active") die("Invalid Password");
+        }else die("Not authorized");
+
+        $criteria = new CDbCriteria();
+        $criteria->order = "saying_datetime DESC";
+        $criteria->limit = 1;
+        $lastSaying = Saying::model()->find($criteria);
+
+        $output = md5($lastSaying->saying_datetime);
+
+        echo json_encode($output, JSON_UNESCAPED_UNICODE);
+    }
+    
+    /**
      * Sends all sayings
      */
     public function actionSayings() {
